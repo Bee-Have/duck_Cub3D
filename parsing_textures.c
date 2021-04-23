1806474a00f14826948f63ea905fd7005d4929dc
user42 <duck.marini@gmail.com>
2021-04-23 10:21:26 +0200

    made sure to close fd of textures after opening it to see if it exists

--- parsing_textures.c
+++ parsing_textures.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 12:02:14 by amarini-          #+#    #+#             */
-/*   Updated: 2021/04/22 17:31:10 by amarini-         ###   ########.fr       */
+/*   Updated: 2021/04/23 10:20:25 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 char	*texture_register(char *str)
 {
 	int		i;
+	int		fd;
 	char	*result;
 
 	i = 0;
@@ -26,7 +27,10 @@ char	*texture_register(char *str)
 	}
 	result = strtrim(str, ft_strlen(str) - i, i);
 	printf("res[%s]\n", result);
-	if (open(result, O_RDONLY) == -1)
+	fd = open(result, O_RDONLY);
+	if (fd == -1)
 		simple_error("Texture path is incorrect or does not exist");
+	else
+		close(fd);
 	return (result);
 }
