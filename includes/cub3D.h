6447f1e99b4e139fe2a36c1abea09e57049ef2f1
Albe MARINI-AUDOUARD <amarini-@e3r13p14.clusters.42paris.fr>
2022-04-25 14:58:05 +0200

    ✨ -Working simple print of map without resize

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -4,12 +4,37 @@
 #include <math.h>
 #include <limits.h>
 #include "libft.h"
-#include "ft_get_file.h"
 #include "mlx.h"
 
 #ifndef CUB3D_H
 #define CUB3D_H
 
+typedef struct s_color
+{
+	unsigned char	a;
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+	unsigned int	code;
+}			t_color;
 
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_pxl;
+	int		line_len;
+	int		endian;
+}			t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*win;
+	t_img	img;
+
+	char	**map;
+}			t_mlx;
 
 #endif
