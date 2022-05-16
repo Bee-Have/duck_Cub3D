d88ec1378af8d82fc54e8fc744d33d7fb4ebb203
Albe MARINI-AUDOUARD <amarini-@e3r10p22.clusters.42paris.fr>
2022-05-16 20:08:55 +0200

    ✨ -Started implementing simple main to test raycasting IN THE FUTURE

---
+++ srcs/init/init_mlx.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_mlx.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 18:09:36 by amarini-          #+#    #+#             */
+/*   Updated: 2022/05/16 20:08:02 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D.h"
+
+t_img	init_img(void)
+{
+	t_img	img;
+	img.addr = NULL;
+	img.bits_pxl = 0;
+	img.line_len = 0;
+	img.endian = 0;
+	return (img);
+}
+
+static void	init_events(t_mlx *mlx)
+{
+	mlx->event.is_w_pressed = 0
+	mlx->event.is_a_pressed = 0
+	mlx->event.is_s_pressed = 0
+	mlx->event.is_d_pressed = 0
+	mlx->event.is_left_pressed = 0
+	mlx->event.is_right_pressed = 0
+	return ;
+}
+
+t_mlx	*init_mlx(int width, int height)
+{
+	t_mlx	*mlx;
+
+	mlx = (t_mlx *)malloc(sizeof(t_mlx));
+	//send error if pb w/ malloc
+	if (!mlx)
+		return (NULL);
+	mlx->mlx = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx, width, height, "cub3D");
+	mlx->img = init_img();
+	mlx->img.img = mlx_new_image(mlx->mlx, width, height);
+	mlx->img.addr = mlx_get_data_addr(mlx->img.img, &mlx->img.bits_pxl
+								, &mlx->img.line_len, &mlx->img.endian);
+	mlx->screen.x = width;
+	mlx->screen.y = height;
+	init_events(mlx);
+	mlx->pj = NULL;
+	mlx->map = NULL;
+	return (mlx);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 16:10:57 by amarini-          #+#    #+#             */
-/*   Updated: 2022/05/16 17:39:41 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/05/16 20:08:35 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int	main(int ac, char *av)
 {
+	t_mlx	*mlx;
+
 	//call parsing
-	//init map
+	if (ac != 2 /*|| parsing function returns bad*/)
+		return (EXIT_FAILURE);
 	//init mlx
+	//!WARNING window size will be given from file
+	mlx = init_mlx(1920, 1080);
+	//init map
+	//!this is temporary because the file will not only include the map
+	mlx->map = ft_get_file(av[1]);
 	//init pj
+	return (EXIT_SUCCESS);
 }
