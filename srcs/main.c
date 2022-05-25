cc87e6dddd7ee1c08bf4d4f4dceca6d8ce19f95e
Albe MARINI-AUDOUARD <amarini-@e2r8p19.clusters.42paris.fr>
2022-05-25 19:21:35 +0200

    🐛 -Trying to debug color errors of raycasting (i feel dumb so far)

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 16:10:57 by amarini-          #+#    #+#             */
-/*   Updated: 2022/05/25 17:27:30 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/05/25 19:20:27 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,10 @@ int	main(int ac, char **av)
 	sys->events = init_events();
 	// init pj
 	sys->pj = init_pj(sys->s_i.map);
+	printf("floor-white-[%c]\n", sys->s_i.floor.code);
+	printf("ceiling-blue-[%c]\n", sys->s_i.ceiling.code);
+	printf("wall_north-red-[%c]\n", sys->s_i.wall_north.code);
+	printf("wall_south-green-[%c]\n", sys->s_i.wall_south.code);
 	// start mlx routine
 	mlx_routine(sys);
 	ft_free_tab((void **)sys->s_i.map);
