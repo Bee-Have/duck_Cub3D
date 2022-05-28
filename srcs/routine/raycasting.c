ad62a85b2977259905872297790100eb42b53c46
amarini- <duck.marini@gmail.com>
2022-05-28 20:13:08 +0200

    🎨 -Re-organizingthe raycasting function

--- srcs/routine/raycasting.c
+++ srcs/routine/raycasting.c
@@ -6,16 +6,57 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 18:30:53 by amarini-          #+#    #+#             */
-/*   Updated: 2022/05/26 18:27:40 by user42           ###   ########.fr       */
+/*   Updated: 2022/05/28 20:01:40 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+void	render_ray(t_system *sys, double perp_wall_dist, int x, int side)
+{
+	t_int2	start;
+	t_int2	end;
+	int		line_height;
+	int		wall_start;
+	int		wall_end;
+	
+	line_height = (int)(sys->s_i.screen.y / perp_wall_dist);
+	wall_start = -line_height / 2 + sys->s_i.screen.y / 2;
+	if (wall_start < 0)
+		wall_start = 0;
+	wall_end = line_height / 2 + sys->s_i.screen.y / 2;
+	if (wall_end >= sys->s_i.screen.y)
+		wall_end = sys->s_i.screen.y - 1;
+	start.x = x;
+	end.x = x;
+	start.y = 0;
+	end.y = wall_start;
+	draw_line(sys, start, end, sys->s_i.ceiling);
+	start.y = wall_end;
+	end.y = sys->s_i.screen.y;
+	draw_line(sys, start, end, sys->s_i.floor);
+	start.y = wall_start;
+	end.y = wall_end;
+	if (side == 1)
+		draw_line(sys, start, end, sys->s_i.wall_west);
+	else
+		draw_line(sys, start, end, sys->s_i.wall_south);
+}
+
+t_vec2	calc_ray_direction(int x, t_system *sys)
+{
+	t_vec2	ray_dir;
+	double	camera_x;
+	
+	camera_x = 2 * x / (double)sys->s_i.screen.x - 1;
+	ray_dir.x = (sys->pj.dir.x - sys->pj.pos.x) + sys->pj.plane.x * camera_x;
+	ray_dir.y = (sys->pj.dir.y - sys->pj.pos.y) + sys->pj.plane.y * camera_x;
+	return (ray_dir);
+}
+
 void	raycasting_routine(t_system *sys)
 {
 	int		x;
-	double	camera_x;
 	t_vec2	ray_dir;
 	t_int2	map;
 	t_vec2	side_dist;
@@ -30,9 +71,7 @@ void	raycasting_routine(t_system *sys)
 		map.x = (int)sys->pj.pos.x;
 		map.y = (int)sys->pj.pos.y;
 		hit = 0;
-		camera_x = 2 * x / (double)sys->s_i.screen.x - 1;
-		ray_dir.x = (sys->pj.dir.x - sys->pj.pos.x) + sys->pj.plane.x * camera_x;
-		ray_dir.y = (sys->pj.dir.y - sys->pj.pos.y) + sys->pj.plane.y * camera_x;
+		ray_dir = calc_ray_direction(x, sys);
 		if (ray_dir.x == 0)
 			delta_dist.x = 1e30;
 		else
@@ -81,37 +120,10 @@ void	raycasting_routine(t_system *sys)
 				|| sys->s_i.map[map.y][map.x] == '1')
 			{
 				hit = 1;
-				double	perp_wall_dist;
-				t_int2	start;
-				t_int2	end;
-				int		line_height;
-				int		draw_start;
-				int		draw_end;
 				if (side == 0)
-					perp_wall_dist = side_dist.x - delta_dist.x;
+					render_ray(sys, side_dist.x - delta_dist.x, x, side);
 				else
-					perp_wall_dist = side_dist.y - delta_dist.y;
-				line_height = (int)(sys->s_i.screen.y / perp_wall_dist);
-				draw_start = -line_height / 2 + sys->s_i.screen.y / 2;
-				if (draw_start < 0)
-					draw_start = 0;
-				draw_end = line_height / 2 + sys->s_i.screen.y / 2;
-				if (draw_end >= sys->s_i.screen.y)
-					draw_end = sys->s_i.screen.y - 1;
-				start.x = x;
-				end.x = x;
-				start.y = 0;
-				end.y = draw_start;
-				draw_line(sys, start, end, sys->s_i.ceiling);
-				start.y = draw_end;
-				end.y = sys->s_i.screen.y;
-				draw_line(sys, start, end, sys->s_i.floor);
-				start.y = draw_start;
-				end.y = draw_end;
-				if (side == 1)
-					draw_line(sys, start, end, sys->s_i.wall_north);
-				else
-					draw_line(sys, start, end, sys->s_i.wall_south);
+					render_ray(sys, side_dist.y - delta_dist.y, x, side);
 			}
 		}
 		++x;
