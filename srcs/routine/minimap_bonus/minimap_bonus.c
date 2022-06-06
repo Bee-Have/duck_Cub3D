6929b24cf04c614dd1dc19db45d84488de4e2011
Albe MARINI-AUDOUARD <amarini-@e2r12p23.clusters.42paris.fr>
2022-06-06 20:07:06 +0200

    🐛 -Fixed minimap cropped by screen issue

--- srcs/routine/minimap_bonus/minimap_bonus.c
+++ srcs/routine/minimap_bonus/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 17:39:49 by amarini-          #+#    #+#             */
-/*   Updated: 2022/06/06 19:47:59 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/06/06 20:05:27 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,11 +62,12 @@ void	minimap_routine(t_system *sys, int corner)
 	pxl_unit = minimap_pxl_unit(sys->s_i.map);
 	area_start = make_int2(0, 0);
 	if (corner == T_RIGHT)
-		area_start.x = W_WIDTH - W_WIDTH / 4;
+		area_start.x = W_WIDTH - ft_strlen(sys->s_i.map[0]) * pxl_unit;
 	else if (corner == B_LEFT)
-		area_start.y = W_HEIGHT - W_HEIGHT / 4;
+		area_start.y = W_HEIGHT - ft_tab_len((void **)sys->s_i.map) * pxl_unit;
 	else if (corner == B_RIGHT)
-		area_start = make_int2(W_HEIGHT - W_HEIGHT / 4, W_WIDTH - W_WIDTH / 4);
+		area_start = make_int2(W_HEIGHT - ft_tab_len((void **)sys->s_i.map) * pxl_unit,
+				W_WIDTH - ft_strlen(sys->s_i.map[0]) * pxl_unit);
 	map_start = make_int2(area_start.y, area_start.x);
 	render_minimap(sys, map_start, pxl_unit);
 	render_pj_minimap(sys, map_start, pxl_unit);
