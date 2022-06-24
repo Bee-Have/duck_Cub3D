1a5c3c75d67be6fde30be59d1650f11d960d994a
Albe MARINI-AUDOUARD <amarini-@e1r13p3.42.fr>
2022-06-24 15:46:54 +0200

    🩹 -Lowered mouse movement speed on the bonus part

--- includes/duckling3d_bonus.h
+++ includes/duckling3d_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   duckling3d_bonus.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:56:34 by user42            #+#    #+#             */
-/*   Updated: 2022/06/22 19:59:21 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/06/24 15:45:29 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "libft.h"
 # include "mlx.h"
 
-# define MOUSE_SPEED 0.02
+# define MOUSE_SPEED 0.001
 
 //* minimap
 void	render_pj_minimap(t_system *sys, t_int2 pj_pos);
