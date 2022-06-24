caccaf8a62aaa401a3ed7e375229ef76d937f987
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-06-24 15:23:23 +0200

    💄 Fix the dir length on minimap.

--- srcs/routine/minimap_bonus/minimap_pj_bonus.c
+++ srcs/routine/minimap_bonus/minimap_pj_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap_pj_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 18:28:28 by amarini-          #+#    #+#             */
-/*   Updated: 2022/06/16 23:38:44 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/06/24 15:22:05 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	render_pj_minimap(t_system *sys, t_int2 pj_pos)
 	size = TILE_SIZE / 2;
 	draw_circle(sys, color, pj_pos, size);
 	middle = make_int2(pj_pos.y + size / 2, pj_pos.x + size / 2);
-	dir.y = sys->pj.dir.y * size * TILE_SIZE + middle.y;
-	dir.x = sys->pj.dir.x * size * TILE_SIZE + middle.x;
+	dir.y = sys->pj.dir.y * TILE_SIZE * 2 + middle.y;
+	dir.x = sys->pj.dir.x * TILE_SIZE * 2 + middle.x;
 	color = make_color(255, 0, 0, 0);
 	draw_line(sys, middle, dir, color);
 }
